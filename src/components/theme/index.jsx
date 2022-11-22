9d2102571a915276ccf5d2db22a467f7d53ad49b
Leonardo Moreira <leo.ms@hotmail.com>
2022-11-22 15:25:36 +0000

    testando useState para função alterar tema

--- src/components/theme/index.jsx
+++ src/components/theme/index.jsx
@@ -3,22 +3,24 @@ import { DefaultTheme } from "./defaultTheme";
 
 
 const listThemes = [DarkTheme, DefaultTheme]
-
 let currentTheme = 0
 
-function changeTheme(currentTheme){
-        
-        
-    return(currentTheme++)
-}
 
+function changeTheme(){
+    let setCurrentTheme = 0
+    function cont (setCurrentTheme){
+        setCurrentTheme++
+    }
+    currentTheme = cont(setCurrentTheme)
+    return(currentTheme)
+}
+console.log(currentTheme)
 export default function ButtonAltTheme(){
     return(
         <button onClick={changeTheme}></button>
     )  
 }
-changeTheme(currentTheme)
-currentTheme = currentTheme
 
-const Theme = listThemes[currentTheme]
+console.log(currentTheme)
+const Theme = listThemes[0]
 export {Theme}
